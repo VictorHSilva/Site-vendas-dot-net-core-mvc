f3143b4145bba96843bfba06e61a1f111f0dcd26
Victor <victorhugo_rosa@hotmail.com>
2019-11-03 18:35:24 -0300

    Criando pagina de erro e formatacao de numeros e datas

--- Controllers/VendedorController.cs
+++ Controllers/VendedorController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -47,13 +48,13 @@ namespace SiteVendas.Controllers
         {
            if(id == null)
            {
-               return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
            }
 
             var obj = _vendedorService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
         }
@@ -71,13 +72,13 @@ namespace SiteVendas.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _vendedorService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
         }
@@ -86,13 +87,13 @@ namespace SiteVendas.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _vendedorService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             List<Departamento> departamentos = _departamentoService.FindAll();
@@ -107,22 +108,28 @@ namespace SiteVendas.Controllers
         {
             if(id != vendedor.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id não correspondem" });
             }
             try
             {     
             _vendedorService.Update(vendedor);
             return RedirectToAction(nameof(Index));
             }
-            catch (NotFoudException)
+            catch (ApplicationException e)
             {
-                return NotFound();
-            }
-            catch(DbConcurrencyException)
-            {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
 
         }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
+            }
     }
 }
